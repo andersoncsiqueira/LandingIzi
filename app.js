dd2a87be634600cb9e66acfaa933f2139357be43
andersoncsiqueira <92267959+andersoncsiqueira@users.noreply.github.com>
2022-05-08 16:18:11 -0300

    2d

---
+++ app.js
@@ -0,0 +1,19 @@
+const infoPLus = document.querySelector('[data-js="plusinfo"]')
+const buttonPlus = document.querySelector('[data-js="plusbutton"]')
+const close = document.querySelector('[data-js="close"]')
+
+
+buttonPlus.addEventListener('click', ()=> {
+
+infoPLus.classList.toggle('off')
+
+
+
+})
+
+close.addEventListener('click', ()=> {
+
+    infoPLus.classList.toggle('off')
+    
+    
+    })
